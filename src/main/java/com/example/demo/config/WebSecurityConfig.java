eeb88790ab5be18e03cfc90173c3c631399df654
Andrey Zemlyachev <kvintilian@gmail.com>
2021-07-02 20:10:10 +0300

    refactor lombok

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -35,15 +36,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         // Use BCryptPasswordEncoder
         auth.userDetailsService(jwtUserDetailsService).passwordEncoder(passwordEncoder());
     }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
     @Bean
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
+
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         // We don't need CSRF for this example
--- src/main/java/com/example/demo/model/JwtResponse.java
+++ src/main/java/com/example/demo/model/JwtResponse.java
@@ -1,15 +1,16 @@
 package com.example.demo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.io.Serial;
 import java.io.Serializable;
+
+@AllArgsConstructor
+@Getter
 public class JwtResponse implements Serializable {
     @Serial
     private static final long serialVersionUID = -8091879091924046844L;
+
     private final String jwttoken;
-    public JwtResponse(String jwttoken) {
-        this.jwttoken = jwttoken;
-    }
-    public String getToken() {
-        return this.jwttoken;
-    }
 }
